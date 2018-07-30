2c4a57d7a718f0ffe5fa6d650542d21189ea52b4
Terminal Node <kaminix@users.noreply.github.com>
2018-07-31 01:11:50 +0200

    Renamed cog class.

--- cogs/owner_cmds.py
+++ cogs/owner_cmds.py
@@ -1,7 +1,7 @@
 import discord, sys, os
 from discord.ext import commands
 
-class TestCog:
+class OwnerCmdsCog:
     def __init__(self, bot):
         self.bot = bot
 
@@ -11,11 +11,11 @@ class TestCog:
     @commands.command(name='restart')
     async def _restart(self, ctx, *kwargs):
         if ctx.author.id == 154516898434908160: # This is my discord user ID. If you're modifying this, change to your ID.
-            await ctx.channel.send(ctx.author.mention + " Yes Dear Leader... I will restart now.")
+            await ctx.send(ctx.author.mention + " Yes Dear Leader... I will restart now.")
             print ('\n') # extra new line after the commandlog() output
             os.execl(sys.executable, sys.executable, *sys.argv)
         else:
-            await ctx.channel.send(ctx.author.mention + " You're not the boss of me, I restart when Terminal wants me to.")
+            await ctx.send(ctx.author.mention + " You're not the boss of me, I restart when Terminal wants me to.")
 
 def setup(bot):
-    bot.add_cog(TestCog(bot))
+    bot.add_cog(OwnerCmdsCog(bot))
