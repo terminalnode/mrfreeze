47119207edcaa68f797b990bebd43efea15f65be
Terminal Node <kaminix@users.noreply.github.com>
2018-07-31 00:22:46 +0200

    !restart implemented.

---
+++ cogs/owner_commands.py
@@ -0,0 +1,21 @@
+import discord, sys, os
+from discord.ext import commands
+
+class TestCog:
+    def __init__(self, bot):
+        self.bot = bot
+
+    ####### restart #######
+    ### RESTART THE Bot ###
+    #######################
+    @commands.command(name='restart')
+    async def _restart(self, ctx, *kwargs):
+        if ctx.author.id == 154516898434908160: # This is my discord user ID. If you're modifying this, change to your ID.
+            await ctx.channel.send(ctx.author.mention + " Yes Dear Leader... I will restart now.")
+            print ('\n') # extra new line after the commandlog() output
+            os.execl(sys.executable, sys.executable, *sys.argv)
+        else:
+            await ctx.channel.send(ctx.author.mention + " You're not the boss of me, I restart when Terminal wants me to.")
+
+def setup(bot):
+    bot.add_cog(TestCog(bot))
