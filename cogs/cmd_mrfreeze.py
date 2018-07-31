83cf22d21dbe50ae33d43b4fdbb0c140ef8ab18a
Terminal Node <kaminix@users.noreply.github.com>
2018-07-31 15:01:47 +0200

    Added help-message.

--- cogs/cmd_mrfreeze.py
+++ cogs/cmd_mrfreeze.py
@@ -8,15 +8,26 @@ class MrFreezeCog():
 
     @commands.command(name='mrfreeze')
     async def _mrfreeze(self, ctx, *args):
+        # If they say the bot sucks, we'll default to the suck_reply.
         you_suck = ('sucks', 'suck', 'blows', 'blow')
-
         suck_reply = False
         for i in args:
             if i in you_suck:
                 suck_reply = True
 
-        if suck_reply:
-            await ctx.send(ctx.author.mention + ' No *you* suck!')
+        # If they're asking for help, we'll default to the help_reply.
+        pls_help = ('help', 'wtf', 'what', 'what\'s')
+        help_reply = False
+        for i in args:
+            if i in pls_help:
+                help_reply = True
+
+        if help_reply:
+            await ctx.send(ctx.author.mention + ' ' +
+            'The command **!mrfreeze** will have me output one of my timeless quotes from Batman & Robin.')
+        elif suck_reply:
+            await ctx.send(ctx.author.mention + ' ' +
+            'I suck? Well you\'re a smud!!')
         else:
             quote = botfunctions.native.mrfreeze()
             quote = quote.replace('Batman', ctx.author.mention)
