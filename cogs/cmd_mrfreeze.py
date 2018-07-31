d7260ff7051dd476cfd131393d503a30f85d65e5
Terminal Node <kaminix@users.noreply.github.com>
2018-07-31 02:52:00 +0200

    (Not so) sassy reply.

--- cogs/cmd_mrfreeze.py
+++ cogs/cmd_mrfreeze.py
@@ -7,11 +7,21 @@ class MrFreezeCog():
         self.bot = bot
 
     @commands.command(name='mrfreeze')
-    async def _mrfreeze(self, ctx):
-        quote = botfunctions.native.mrfreeze()
-        quote = quote.replace('Batman', ctx.author.mention)
-        quote = quote.replace('Gotham', '**' + ctx.guild.name + '**')
-        await ctx.send(quote)
+    async def _mrfreeze(self, ctx, *args):
+        you_suck = ('sucks', 'suck', 'blows', 'blow')
+
+        suck_reply = False
+        for i in args:
+            if i in you_suck:
+                suck_reply = True
+
+        if suck_reply:
+            await ctx.send(ctx.author.mention + ' No *you* suck!')
+        else:
+            quote = botfunctions.native.mrfreeze()
+            quote = quote.replace('Batman', ctx.author.mention)
+            quote = quote.replace('Gotham', '**' + ctx.guild.name + '**')
+            await ctx.send(quote)
 
 def setup(bot):
     bot.add_cog(MrFreezeCog(bot))
