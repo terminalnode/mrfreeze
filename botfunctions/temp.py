6877a907d33a34658f49c234be1b0530a8b4f9ac
Terminal Node <kaminix@users.noreply.github.com>
2018-08-01 21:04:32 +0200

    More preposition patterns for temperature conversion.

--- botfunctions/temp.py
+++ botfunctions/temp.py
@@ -66,7 +66,7 @@ async def convert(ctx, tempstatement):
     manual_conversion = False
     try:
         unit_regex = ' (?:°?c(elcius)?(ivilized( units)?)?(u)?(ivilised( units)?)?|°?f(ahrenheit)?(reedom( units)?)?(u)?|°?k(elvin)?|°?r(ankine)?)[^\w]'
-        dest_unit = re.search('(?:in|as)' + unit_regex, ctx.message.content.lower() + ' ').group().strip()
+        dest_unit = re.search('(?:in|as|to|for)' + unit_regex, ctx.message.content.lower() + ' ').group().strip()
         dest_unit = re.search('\s\w+', dest_unit).group().strip()[0]
         manual_conversion = True
     except:
