eb3537bf2a89f50bb822ae2d873a8b4d66a92fdf
Terminal Node <kaminix@users.noreply.github.com>
2018-08-10 06:18:06 +0200

    !readme now produces a snazzy embed.

--- cogs/cmds_links.py
+++ cogs/cmds_links.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
 
-# This cog is meant for things like posting a link to github, posting a link to the readme etc.
-# TODO: Make the links embedded: https://leovoel.github.io/embed-visualizer/
+# Useful links for testing out embeds:
+# https://leovoel.github.io/embed-visualizer/
+# https://cog-creators.github.io/discord-embed-sandbox/
 
 class BragCog():
     def __init__(self, bot):
@@ -10,9 +11,13 @@ class BragCog():
 
     @commands.command(name='readme')
     async def _readme(self, ctx, *args):
-        await ctx.send('This may be more information than you\'re looking for, ' +
-                               'but here\'s a link to the readme for you:\n' +
-                               '<https://github.com/kaminix/MrFreezeRW/blob/master/README.md>')
+        readme = discord.Embed(color=0x00dee9)
+        readme.set_thumbnail(url='https://i.imgur.com/ltGWhQk.png')
+        readme.add_field(name = 'Readme',
+                         value = ('There may be more information here than you\'re looking for, ' +
+                                 'but my readme-file is available [here on Github](https://github.com/kaminix/MrFreezeRW/blob/master/README.md)'))
+        await ctx.send(embed=readme)
+
 
     @commands.command(name='source', aliases=['github'])
     async def _source(self, ctx, *args):
@@ -23,6 +28,7 @@ class BragCog():
                                '[Github](https://github.com/kaminix/MrFreezeRW)!')
         await ctx.send(embed=source)
 
+
     @commands.command(name='dummies')
     async def _dummies(self, ctx, *args):
         # Robin avatar URL:
@@ -30,16 +36,15 @@ class BragCog():
         # Ba'athman avatar URL:
         # https://cdn.discordapp.com/avatars/469030362119667712/7d6ecde5d7c0b85231102aa5b4286563.webp?size=1024
 
-        dummies = discord.Embed(color=0x00dee9)
+        dummies = discord.Embed(color=0x00dee9,
+                                description = ('Here are the links for inviting Ba\'athman and Robin, my arch enemies, to a server.\n\n' +
+                                               'They don\'t do anything at all, but are very useful for testing kick and ban commands.'))
         dummies.set_thumbnail(url="https://i.imgur.com/r4MrUbX.png")
-        dummies.add_field(name='Ba\'athman:',
-                          value='[Invite Ba\'athman](https://discordapp.com/oauth2/authorize?client_id=469030362119667712&scope=bot)\n',
-                          inline=False)
-        dummies.add_field(name='Robin:',
-                          value='[Invite Robin](https://discordapp.com/oauth2/authorize?client_id=469030900492009472&scope=bot)',
-                          inline=False)
+        dummies.add_field(name='Dummy Bots',
+                          value=('[Invite Ba\'athman to a server.](https://discordapp.com/oauth2/authorize?client_id=469030362119667712&scope=bot)\n' +
+                                 '[Invite Robin to a server.](https://discordapp.com/oauth2/authorize?client_id=469030900492009472&scope=bot)'))
 
-        await ctx.send('Here are the links for inviting Ba\'athman and Robin, my arch enemies, to a server:', embed=dummies)
+        await ctx.send(embed=dummies)
 
 def setup(bot):
     bot.add_cog(BragCog(bot))
