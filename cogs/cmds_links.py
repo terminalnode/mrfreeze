bf7b6f5646933b9353dc919b85791364429b2eb9
Terminal Node <kaminix@users.noreply.github.com>
2018-08-11 03:47:55 +0200

    Converted to use the new native.get_image()

--- cogs/cmds_links.py
+++ cogs/cmds_links.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from botfunctions import native
 
 # Useful links for testing out embeds:
 # https://leovoel.github.io/embed-visualizer/
@@ -12,7 +13,7 @@ class BragCog():
     @commands.command(name='readme')
     async def _readme(self, ctx, *args):
         readme = discord.Embed(color=0x00dee9)
-        readme.set_thumbnail(url='https://i.imgur.com/ltGWhQk.png')
+        readme.set_thumbnail(url=native.get_image('ReadMe'))
         readme.add_field(name = 'Readme',
                          value = ('There may be more information here than you\'re looking for, ' +
                                  'but my readme-file is available [here on Github](https://github.com/kaminix/MrFreezeRW/blob/master/README.md)'))
@@ -22,7 +23,7 @@ class BragCog():
     @commands.command(name='source', aliases=['github'])
     async def _source(self, ctx, *args):
         source = discord.Embed(color=0x00dee9)
-        source.set_thumbnail(url='https://i.imgur.com/sVP6w67.png')
+        source.set_thumbnail(url=native.get_image('Source'))
         source.add_field(name='Source code',
                          value='My source code is available on ' +
                                '[Github](https://github.com/kaminix/MrFreezeRW)!')
@@ -39,7 +40,7 @@ class BragCog():
         dummies = discord.Embed(color=0x00dee9,
                                 description = ('Here are the links for inviting Ba\'athman and Robin, my arch enemies, to a server.\n\n' +
                                                'They don\'t do anything at all, but are very useful for testing kick and ban commands.'))
-        dummies.set_thumbnail(url="https://i.imgur.com/r4MrUbX.png")
+        dummies.set_thumbnail(url=native.get_image('Dummies'))
         dummies.add_field(name='Dummy Bots',
                           value=('[Invite Ba\'athman to a server.](https://discordapp.com/oauth2/authorize?client_id=469030362119667712&scope=bot)\n' +
                                  '[Invite Robin to a server.](https://discordapp.com/oauth2/authorize?client_id=469030900492009472&scope=bot)'))
