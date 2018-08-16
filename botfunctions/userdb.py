7218ad6a69e259941eaf33ae4ce185030cd9c732
Terminal Node <kaminix@users.noreply.github.com>
2018-08-16 17:07:00 +0200

    Can now add names/shortcuts to quotes.

--- botfunctions/userdb.py
+++ botfunctions/userdb.py
@@ -1,42 +1,53 @@
 import sqlite3, discord
 from sqlite3 import Error
 
-# This file manages the userdb where we keep stuff like rps scores, mutes,
-# etc. We also have a register of all channels and users which is constantly
-# updated as new information is provided.
+### This file manages the userdb where we keep stuff like rps scores, mutes,
+### etc. We also have a register of all channels and users which is constantly
+### updated as new information is provided.
 
-# This short function creates the connection to the database.
-# It's used in almost every function so it's useful to have.
+### This short function creates the connection to the database.
+### It's used in almost every function so it's useful to have.
 def connect_to_db():
     db_file = 'databases/user.db'
     conn = sqlite3.connect(db_file)
     return conn
 
-# This function creates the database if it doesn't
-# already exists, then closes the connection.
+### This function creates the database if it doesn't
+### already exists, then closes the connection.
 def create():
     conn = connect_to_db()
 
-    # The tables are arranged by the foreign keys they are using.
-    # 'servers' is essentially the top table. crt is short for create,
-    # tbl is short for table.
+    ### The tables are arranged by the foreign keys they are using.
+    ### 'servers' is essentially the top table.
+    ###
+    ### Full list of current tables:
+    ### servers     (server ids and names)
+    ### channels    (channel ids and names)
+    ### users       (users and the nicks they use on different servers)
+    ### quotes      (quote entries, duh)
+    ### rps         (rock, paper, scissors)
+    ### mutes       (list of people with antarctica-tag and why they have them)
+    ### region_bl   (blacklist from using the region command)
 
-    # Server names.
-    crt_servers_tbl   = """   CREATE TABLE IF NOT EXISTS servers(
+    ### Server names.
+    servers_tbl   = """
+                    CREATE TABLE IF NOT EXISTS servers(
                         id integer PRIMARY KEY,
                         name text NOT NULL
-    );"""
+                    );"""
 
-    # Channel IDs/Names.
-    crt_channels_tbl  = """   CREATE TABLE IF NOT EXISTS channels(
+    ### Channel IDs/Names.
+    channels_tbl  = """
+                    CREATE TABLE IF NOT EXISTS channels(
                         id integer PRIMARY KEY,
                         server integer NOT NULL,
                         name text NOT NULL,
                         FOREIGN KEY (server) REFERENCES servers (id)
-    );"""
+                    );"""
 
-    # List of user IDs/Names.
-    crt_users_tbl     = """   CREATE TABLE IF NOT EXISTS users(
+    ### List of user IDs/Names.
+    users_tbl     = """
+                    CREATE TABLE IF NOT EXISTS users(
                         id integer NOT NULL,
                         server integer NOT NULL,
                         disp_name text NOT NULL,
@@ -45,10 +56,11 @@ def create():
                         discriminator integer NOT NULL,
                         FOREIGN KEY (server) REFERENCES servers (id),
                         CONSTRAINT server_user PRIMARY KEY (id, server)
-    );"""
+                    );"""
 
-    # Quotes database.
-    crt_quotes_tbl    = """  CREATE TABLE IF NOT EXISTS quotes(
+    ### Quotes database.
+    quotes_tbl    = """
+                    CREATE TABLE IF NOT EXISTS quotes(
                         id integer PRIMARY KEY NOT NULL,
                         quoter integer NOT NULL,
                         quotee integer NOT NULL,
@@ -56,49 +68,69 @@ def create():
                         channel integer NOT NULL,
                         date_said text NOT NULL,
                         content text NOT NULL,
-                        shortcut text,
+                        name text,
                         FOREIGN KEY (quoter) REFERENCES users (id),
                         FOREIGN KEY (quotee) REFERENCES users (id),
                         FOREIGN KEY (server) REFERENCES servers (id),
                         FOREIGN KEY (channel) REFERENCES channels (id)
-    );"""
+                    );"""
 
-    # Rock, Paper, Scissors stats.
-    crt_rps_tbl       = """   CREATE TABLE IF NOT EXISTS rps(
-                        id integer PRIMARY KEY NOT NULL,
+    ### Rock, Paper, Scissors stats.
+    rps_tbl       = """
+                    CREATE TABLE IF NOT EXISTS rps(
+                        id integer NOT NULL,
+                        server integer NOT NULL,
                         wins integer NOT NULL,
                         losses integer NOT NULL,
                         drawn integer NOT NULL,
-                        FOREIGN KEY (id) REFERENCES users (id)
-    );"""
+                        FOREIGN KEY (id) REFERENCES users (id),
+                        FOREIGN KEY (server) REFERENCES servers (id),
+                        CONSTRAINT server_user PRIMARY KEY (id, server)
+                    );"""
 
-    # If a user gets antarctica'd, that's registered here.
-    # A user can self-banish, in which case voluntary will be
-    # True and they're allowed to unbanish themselves as well.
-    crt_mutes_tbl     = """   CREATE TABLE IF NOT EXISTS mutes(
-                        user integer NOT NULL,
+    ### If a user gets antarctica'd, that's registered here.
+    ### A user can self-banish, in which case voluntary will be
+    ### True and they're allowed to unbanish themselves as well.
+    mutes_tbl     = """
+                    CREATE TABLE IF NOT EXISTS mutes(
+                        id integer NOT NULL,
+                        server integer NOT NULL,
                         until date NOT NULL,
                         voluntary boolean NOT NULL,
-                        FOREIGN KEY (user) REFERENCES users (id)
-    );"""
+                        FOREIGN KEY (id) REFERENCES users (id),
+                        FOREIGN KEY (server) REFERENCES servers(id),
+                        CONSTRAINT server_user PRIMARY KEY (id, server)
+                    );"""
 
-    # Here we'll create all the tables.
+    ### A user can get banned from changing their region if
+    ### mods detect abuse. They will then be put on this blacklist.
+    region_bl_tbl = """
+                    CREATE TABLE IF NOT EXISTS region_bl(
+                        id integer NOT NULL,
+                        server integer NOT NULL,
+                        FOREIGN KEY (id) REFERENCES users (id),
+                        FOREIGN KEY (server) REFERENCES servers (id),
+                        CONSTRAINT server_user PRIMARY KEY (id, server)
+                    );"""
+
+    ### Here we'll create all the tables.
     with conn:
         try:
             c = conn.cursor()
-            c.execute(crt_servers_tbl)
-            c.execute(crt_channels_tbl)
-            c.execute(crt_users_tbl)
-            c.execute(crt_quotes_tbl)
-            c.execute(crt_rps_tbl)
-            c.execute(crt_mutes_tbl)
+            c.execute(servers_tbl)
+            c.execute(channels_tbl)
+            c.execute(users_tbl)
+            c.execute(quotes_tbl)
+            c.execute(rps_tbl)
+            c.execute(mutes_tbl)
+            c.execute(region_bl_tbl)
 
-        # If an error occurs, it will be reported to the terminal with the
-        # error printed.
+        ### If an error occurs, it will be reported to the terminal with the
+        ### error printed.
         except Error as e:
             print ('Unable to create user.db quotes table\n' + str(e))
 
-# Create or update server/channel/user/etc.
+### This function creates/updates the db entry for a certain server/guild.
 def fix_server(guild):
     conn = connect_to_db()
     id = guild.id
@@ -117,7 +149,7 @@ def fix_server(guild):
         if server_exists:
             sql = '''
                   UPDATE servers
-                    SET name = ?
+                  SET name = ?
                   WHERE id = ?
                   '''
 
@@ -130,6 +162,7 @@ def fix_server(guild):
 
         c.execute(sql, (name, id))
 
+### This function creates/updates the db entry for a certain channel.
 def fix_channel(channel):
     conn = connect_to_db()
     channel_id = channel.id
@@ -151,9 +184,9 @@ def fix_channel(channel):
         if channel_exists:
             sql = '''
                   UPDATE channels
-                    SET name = ?,
-                        server = ?
-                    WHERE id = ?
+                  SET name = ?,
+                      server = ?
+                  WHERE id = ?
                   '''
 
         # If the channel doesn't exist, we'll create it.
@@ -165,7 +198,7 @@ def fix_channel(channel):
 
         c.execute(sql, (name, server_id, channel_id))
 
-
+### This function creates/updates the db entry for a certain user in a certain server.
 def fix_user(ctx):
     conn = connect_to_db()
 
@@ -192,11 +225,11 @@ def fix_user(ctx):
         if user_exists:
             sql = '''
                   UPDATE users
-                    SET disp_name = ?,
-                        discriminator = ?,
-                        name = ?,
-                        avatar = ?
-                    WHERE id = ? AND server = ?
+                  SET disp_name = ?,
+                      discriminator = ?,
+                      name = ?,
+                      avatar = ?
+                  WHERE id = ? AND server = ?
                   '''
 
         if not user_exists:
@@ -210,17 +243,25 @@ def fix_user(ctx):
         except Error as e:
             print(e)
 
-
+### This function creates/updates the db entry for a certain users mute status in a certain server.
 def fix_mute(user, duration, is_delete):
     conn = connect_to_db()
 
+### This function creates/updates the db entry for a certain user in a certain server.
 def fix_rps(user):
     conn = connect_to_db()
+##########
+### Quote-related commands.
+##########
 
-# Quote-related commands.
+### This function returns an embed with the requested quote
+### which can then be posted to the chat and looks beautiful.
 def quote_embed(db_entry):
     conn = connect_to_db()
-    db_entry = db_entry[0]
+
+    ### [ ( db_entry ) ] -> ( db_entry )
+    db_entry   = db_entry[0]
+    ### Unpacking (db_entry) into some variables we'll be using.
     id         = db_entry[0]
     quoter_id  = db_entry[1]
     quotee_id  = db_entry[2]
@@ -229,7 +270,8 @@ def quote_embed(db_entry):
     date_said  = db_entry[5]
     content    = db_entry[6]
 
-    # Getting some variables.
+    ### Using the quoter and quotee information, we can
+    ### Retrieve some additional information from the users db
     with conn:
         c = conn.cursor()
         u_sql = ''' SELECT * FROM users WHERE id = ? AND server = ? '''
@@ -238,31 +280,29 @@ def quote_embed(db_entry):
         c.execute(u_sql, (quotee_id, server_id))
         quotee_tuple = c.fetchall()[0]
 
-    # dname is displayname
+    ### The following is the information we'll be getting:
+    ### DNAME    = display_name
+    ### HASHNAME = NAME#DISCRIMINATOR (eg. TerminalNode#5986)
+    ### AVATAR   = URLs for the quoter/quotee avatars.
     quoter_dname = quoter_tuple[2]
     quotee_dname = quotee_tuple[2]
-    # hashname is user_name#discriminator, ex. TerminalNode#5986
     quoter_hashname = (quoter_tuple[3] + '#' + str(quoter_tuple[5]))
     quotee_hashname = (quotee_tuple[3] + '#' + str(quotee_tuple[5]))
-    # avatar urls
     quoter_avatar = quoter_tuple[4]
     quotee_avatar = quotee_tuple[4]
 
-    # combined_name is dname (hashname)
-    # Super Terminal (TerminalNode#5986)
-    # quoter_combined_name = ('%s (%s)' % (quoter_dname, quoter_hashname))
-    # quotee_combined_name = ('%s (%s)' % (quotee_dname, quotee_hashname))
-
-
     embed = discord.Embed(color=0x00dee9)
-    embed.set_author(name = quotee_dname, icon_url = quotee_avatar)
-    embed.add_field(name = ('Posted on: ' + date_said), value = content)
+    # embed.set_author(name = quotee_dname)
+    embed.set_thumbnail(url=quotee_avatar)
+    embed.add_field(name = (quotee_dname + ', ' + date_said), value = (content + '\n\u200b'))
     embed.set_footer(icon_url = quoter_avatar, text=("Added by " + quoter_dname + ' (Quote ID: ' + str(id) + ')'))
 
     return embed
 
-
-
+### This function creates a new quote entry.
+### The input required is ctx from !quote-command
+### and the message quoted (which will be retrived
+### from a provided ID via !quote, not here).
 def crt_quote(ctx, quote):
     conn = connect_to_db()
     id        = quote.id
@@ -270,16 +310,12 @@ def crt_quote(ctx, quote):
     quotee    = quote.author.id
     server    = quote.guild.id
     channel   = quote.channel.id
-    date_said = '{:%Y-%m-%d at %H:%M}'.format(quote.created_at)
+    date_said = '{:%Y-%m-%d}'.format(quote.created_at)
     content   = quote.content
 
-    print ('Check: crt_quote 1')
     fix_user(ctx)
-    print ('Check: crt_quote 2')
     fix_user(quote)
-    print ('Check: crt_quote 3')
     fix_channel(quote.channel)
-    print ('Check: crt_quote 4')
 
     with conn:
         c = conn.cursor()
@@ -291,17 +327,13 @@ def crt_quote(ctx, quote):
         if len(fetch) == 0:
             quote_exists = False
 
-        print('Check: crt_quote 5')
-
         if not quote_exists:
             sql =   '''
                     INSERT INTO quotes (id, quoter, quotee, server, channel, date_said, content)
                     VALUES (?,?,?,?,?,?,?)
                     '''
             try:
-                print ('Check: crt_quote 6')
                 c.execute(sql, (id, quoter, quotee, server, channel, date_said, content))
-                print ('Check: crt_quote 7')
                 c.execute('SELECT * FROM quotes WHERE id = ?', (c.lastrowid,))
                 new_entry = c.fetchall()
             except Error as e:
@@ -309,15 +341,54 @@ def crt_quote(ctx, quote):
 
     # Now we're ready for out return values
     if not quote_exists:
-        print ('Check: crt_quote 8 (quote created!)')
-        print ('New entry: ', new_entry)
         return quote_embed(new_entry)
     else:
-        print ('Check: crt_quote 8 (quote not created)')
         return False
 
-def get_quote_id(id, name):
+### This function assigns the optional field 'name' to a given quote.
+def name_quote(id, name):
     conn = connect_to_db()
 
-def get_quote_rnd(id, is_name):
+    with conn:
+        c = conn.cursor()
+
+        q_quote = ''' SELECT * FROM quotes WHERE id = ? '''
+        c.execute(q_quote, (id,))
+        fetch = c.fetchall()
+
+        # The following variables are necessary
+        # to know what went wrong, if anything.
+        found_quote = False
+        updated_quote = False
+        old_name = None
+        if len(fetch) != 0:
+            found_quote = True
+            old_name = fetch[0][7]
+
+            sql =   '''
+                    UPDATE quotes
+                    SET name = ?
+                    WHERE id = ?
+                    '''
+
+            if old_name != name:
+                try:
+                    c.execute(sql, (name, id))
+                    updated_quote = True
+                except Error as e:
+                    print (e)
+
+    return (found_quote, updated_quote, old_name)
+
+### This function retrieves a quote based on an ID or name.
+### Quote is then sent to the quote_embed()-function (if
+### a quote is found) and returns the resulting embed.
+def get_quote_id(id):
+    conn = connect_to_db()
+
+### This function retrieves a random quote from the dictionary.
+### Can optionally look for a random quote by a certain user.
+### Quote is then sent to quote_embed()-function (if a quote
+### is found) and returns the resulting embed.
+def get_quote_rnd(id, user):
     conn = connect_to_db()
--- cogs/cmd_quote.py
+++ cogs/cmd_quote.py
@@ -41,31 +41,57 @@ class QuoteCmdCog:
                 try:
                     msg = await channel.get_message(id)
                 except:
-                    pass
+                    pass # this means the id wasn't in that channel
 
             if msg != False:
                 new_quote = userdb.crt_quote(ctx, msg)
-                await ctx.send(embed=new_quote)
+                if new_quote != False:
+                    await ctx.send(embed=new_quote)
+                else:
+                    await ctx.send(ctx.author.mention + ' That quote is already in the database.')
             else:
-                await ctx.send(ctx.author.mention + ' I wasn\'t able to find any quote with the id: ' + str(id))
+                await ctx.send(ctx.author.mention + ' I wasn\'t able to find any post with the id: ' + str(id))
 
-        def name_quote():
+        async def name_quote(id, name):
+            found, updated, old_name = userdb.name_quote(id, name)
+
+            if found and updated:
+                # Alternative 1:
+                # All went smoothly.
+                if old_name != None and old_name != str():
+                    await ctx.send(ctx.author.mention + ' The shortcut was changed from **' + str(old_name) + '** to **' + name + '**!')
+                else:
+                    await ctx.send(ctx.author.mention + ' The shortcut **' + name + '** was assigned to the requested quote.')
+
+            elif found and not updated:
+                # Alternative 2:
+                # Found but couldn't update.
+                if old_name == name:
+                    await ctx.send(ctx.author.mention + ' That quote already has the shortcut **' + name + '**!')
+                else:
+                    await ctx.send(ctx.author.mention + ' I found the quote, but for some reason I wasn\'t able to update the name/shortcut.')
+
+            elif not found:
+                # Alternative 3:
+                # The quote wasn't found.
+                await ctx.send(ctx.author.mention + ' I can\'t find that quote and thus can\'t assign a name/shortcut to it!')
+
+        async def remove_quote():
             pass
 
-        def remove_quote():
+        async def random_quote():
             pass
 
-        def random_quote():
-            pass
-
-        def read_quote():
+        async def read_quote():
             pass
 
         # Finally, we'll try to execute one of the above functions.
         if len(args) == 0:
             pass
 
-        await add_quote(479269483219910668)
+        # tested:
+        # await add_quote(479269483219910668)
+        # await name_quote(479269483219910668, 'test3')
 
 
 
