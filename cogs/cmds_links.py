b6fdd55a927f5e677b70a43aab347f0d8f4387e4
Terminal Node <kaminix@users.noreply.github.com>
2018-08-19 22:36:30 +0200

    Removed superfluous URLs

--- cogs/cmds_links.py
+++ cogs/cmds_links.py
@@ -32,11 +32,6 @@ class LinksCog():
 
     @commands.command(name='dummies')
     async def _dummies(self, ctx, *args):
-        # Robin avatar URL:
-        # https://cdn.discordapp.com/avatars/469030900492009472/5326bec1dc7aa3f20b0b1651070fc069.webp?size=1024
-        # Ba'athman avatar URL:
-        # https://cdn.discordapp.com/avatars/469030362119667712/7d6ecde5d7c0b85231102aa5b4286563.webp?size=1024
-
         dummies = discord.Embed(color=0x00dee9,
                                 description = ('Here are the links for inviting Ba\'athman and Robin, my arch enemies, to a server.\n\n' +
                                                'They don\'t do anything at all, but are very useful for testing kick and ban commands.'))
