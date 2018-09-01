05a22116ffc6ab79285286c9a45ab8057ac3110a
Terminal Node <kaminix@users.noreply.github.com>
2018-09-01 02:37:26 +0200

    is_mod check created.

--- botfunctions/checks.py
+++ botfunctions/checks.py
@@ -1,10 +1,15 @@
 # Here we define all of our custom checks.
+import discord
 
 async def is_owner(ctx):
+    # @commands.check(checks.is_owner)
     if ctx.author.id != 154516898434908160:
-        await ctx.send(ctx.author.mention +
-                       ' You\'re not the boss of me, only Terminal is allowed to issue that command.')
+        await ctx.send(ctx.author.mention + ' You\'re not the boss of me, only Terminal is allowed to issue that command.')
     return ctx.author.id == 154516898434908160
 
 async def is_mod(ctx):
-    return (discord.utils.get(ctx.guild.roles, name='Administration') in ctx.author.roles)
+    # @commands.check(checks.is_mod)
+    mod_status = (discord.utils.get(ctx.guild.roles, name='Administration') in ctx.author.roles)
+    if not mod_status:
+        await ctx.send(ctx.author.mention + ' Only mods are allowed to use that command.')
+    return mod_status
