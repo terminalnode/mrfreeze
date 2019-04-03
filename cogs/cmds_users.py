67c3f4c845d5b4e89e33a696b5dd7c737cb02fd1
Alexander <terminalnode@protonmail.com>
2019-04-03 14:55:11 +0200

    Moved !rules into moderation cog.

--- cogs/cmds_users.py
+++ cogs/cmds_users.py
@@ -22,41 +22,6 @@ class UserCmdsCog(commands.Cog):
             'Antarctica':       discord.utils.get(guild.roles, name='Antarctica')
             }
 
-    @commands.command(name='rules', aliases=['rule'])
-    async def _rules(self, ctx, *args):
-        request = str()
-        for i in args:
-            request += (i.lower())
-
-        rules = {
-        1: ( '1', 'topic', 'ontopic', 'offtopic' ),
-        2: ( '2', 'civil', 'disagreement' ),
-        3: ( '3', 'dismissive', 'opinion', 'opinions' ),
-        4: ( '4', 'joke', 'jokes', 'joking', 'sex', 'sexual',
-             'orientation', 'weight', 'race', 'skin', 'color',
-             'gender', 'colour' ),
-        5: ( '5', 'shoe', 'shoes', 'age', 'mature', 'maturity', 'shoesize', 'act' ),
-        6: ( '6', 'spam', 'nonsense' ),
-        7: ( '7', 'benice', 'nice' )
-        }
-
-        called_rules = list()
-
-        # If 'all' is in the request we'll just call all rules.
-        # If not we'll see if any of the keywords are in the request.
-        if 'all' in request:
-            called_rules = [ 1, 2, 3, 4, 5, 6, 7 ]
-        else:
-            for rule_no in rules:
-                for keyword in rules[rule_no]:
-                    if keyword in request and rule_no not in called_rules:
-                        called_rules.append(rule_no)
-
-        if len(called_rules) == 0:
-            replystr = 'Sorry %s, your terms don\'t seem to match any rules. :thinking:'
-            await ctx.send(replystr % (ctx.author.mention,))
-        else:
-            await ctx.send(ctx.author.mention + '\n' + native.get_rule(called_rules))
 
     @commands.command(name='mrfreeze', aliases=['freeze'])
     async def _mrfreeze(self, ctx, *args):
