d7e81cc625763f9fe8551bde8222333661b96592
Alexander <terminalnode@protonmail.com>
2019-04-04 16:39:18 +0200

    Will now show an updated member count when a user leaves.

--- __main__.py
+++ __main__.py
@@ -166,11 +166,12 @@ async def on_message(message):
 async def on_member_remove(member):
     mod_channel = discord.utils.get(member.guild.channels, name='mod-discussion')
     member_name = str(member.name + '#' + str(member.discriminator))
+    member_count = str(len(member.guild.members))
     embed = discord.Embed(color=0x00dee9)
     embed.set_thumbnail(url=member.avatar_url)
     embed.add_field( name='A member has left the server! :sob:',
-                     value=('**%s#%s** is a trechorous smud who\'s turned their back on %s.' %
-                     (member.name, str(member.discriminator), member.guild.name)) )
+                     value=('**%s#%s** is a trechorous smud who\'s turned their back on %s. We now have %s members.' %
+                     (member.name, str(member.discriminator), member.guild.name, member_count)) )
     await mod_channel.send(embed=embed)
 
 
