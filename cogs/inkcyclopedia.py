45505db8949381881da10043ebb49a4d13ed0a19
Alexander <terminalnode@protonmail.com>
2019-04-04 14:37:25 +0200

    Added useful comment

--- cogs/inkcyclopedia.py
+++ cogs/inkcyclopedia.py
@@ -2,6 +2,10 @@ import discord
 from discord.ext import commands
 from botfunctions import native
 
+# Small script listening to all incoming messages looking for
+# mentions of inks. Based on The Inkcyclopedia by klundtasaur:
+# https://www.reddit.com/r/fountainpens/comments/5egjsa/klundtasaurs_inkcyclopedia_for_rfountainpens/
+
 class InkcyclopediaCog(commands.Cog, name='Inkcyclopedia'):
     """Type an ink inside {curly brackets} and I'll tell you what it looks like!"""
     def __init__(self, bot):
