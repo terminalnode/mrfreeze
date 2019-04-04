9390486c4ddb6392332603b6663659487d7010d1
Alexander <terminalnode@protonmail.com>
2019-04-04 17:13:12 +0200

    Tiny fixes.

--- cogs/error_handling.py
+++ cogs/error_handling.py
@@ -2,15 +2,10 @@ import discord, re, traceback
 from discord.ext import commands
 from botfunctions import errorhandling
 
-# Small script listening to all incoming messages looking for
-# mentions of inks. Based on The Inkcyclopedia by klundtasaur:
-# https://www.reddit.com/r/fountainpens/comments/5egjsa/klundtasaurs_inkcyclopedia_for_rfountainpens/
-
 class ErrorHandlerCog(commands.Cog, name='ErrorHandler'):
     """How the bot acts when errors occur."""
     def __init__(self, bot):
         self.bot = bot
-        self.inkydb = list()
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
