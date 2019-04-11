e9fcbbbb54efd3bf4b872235792d4fc0f5da9eca
Alexander <terminalnode@protonmail.com>
2019-04-11 23:37:48 +0200

    Consolidated all pinshandling to cog.

--- cogs/pin_handling.py
+++ cogs/pin_handling.py
@@ -1,6 +1,6 @@
 import discord, asyncio
 from discord.ext import commands
-from internals import pinlist
+from internals import var
 
 class PinHandlerCog(commands.Cog, name='PinHandler'):
     """Posts the content of a message that was just pinned to chat."""
@@ -10,24 +10,36 @@ class PinHandlerCog(commands.Cog, name='PinHandler'):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        # Creating dict of all pins in channels in the guilds.
-        # pinsDict is first set to None so we know that it's not done
-        # if there's a pin made while the bot is loading.
-        self.pinsDict = await pinlist.create_dict(self.bot.guilds)
+        pinsDict = dict()
+        for guild in self.bot.guilds:
+            pinsDict[guild.id] = dict()
+            for channel in guild.channels:
+                if isinstance(channel, discord.channel.TextChannel):
+                    try:
+                        pinsDict[guild.id][channel.id] = len(await channel.pins())
+                        print( f"{var.cyan}Fetched pins from {var.red}{guild.name} {var.green}{channel.name}{var.reset}" )
+
+                        if channel.id == 466241532458958850 and len(self.bot.guilds) == 2:
+                            # This is for debugging purposes because the dict takes forever to build.
+                            self.pinsDict = pinsDict
+                            print (f"{var.boldcyan}PinsDict all done!{var.reset}")
+                            return
+                    except: pass # Channel probably got deleted.
+
+        self.pinsDict = pinsDict
+        print (f"{var.boldcyan}PinsDict all done!{var.reset}")
 
     @commands.Cog.listener()
     async def on_guild_channel_pins_update(self, channel, last_pin):
         # Unfortunately we have to cast an empty return
         # if the dict isn't finished yet.
         if self.pinsDict == None:
-            print ('The PinsDict isn\'t finished yet!')
+            print (f"{var.cyan}The {var.red}pinsDict{var.cyan} isn't finished yet!{var.reset}")
             return
 
         # The channel might be new, if so we need to create an entry for it.
-        try:
-            self.pinsDict[channel.guild.id][channel.id]
-        except KeyError:
-            self.pinsDict[channel.guild.id][channel.id] = 0
+        try:                self.pinsDict[channel.guild.id][channel.id]
+        except KeyError:    self.pinsDict[channel.guild.id][channel.id] = 0
 
         # For comparisson between the two. These numbers will be
         # used to determine whether a pin was added or removed.
@@ -39,9 +51,8 @@ class PinHandlerCog(commands.Cog, name='PinHandler'):
 
         # If a pin was added when the bot was starting up, this won't work.
         # But it will work the next time as the pinsDict is updated.
-        was_added = False
-        if new_pins > old_pins:
-            was_added = True
+        if new_pins > old_pins: was_added = True
+        else:                   was_added = False
 
         # Updating the list of pins.
         self.pinsDict[channel.guild.id][channel.id] = new_pins
@@ -52,8 +63,7 @@ class PinHandlerCog(commands.Cog, name='PinHandler'):
             pinned_message.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
 
             # Attaching first attachment of the post, if there are any.
-            if message.attachments:
-                pinned_message.set_image(url=message.attachments[0].url)
+            if message.attachments: pinned_message.set_image(url=message.attachments[0].url)
 
             channel_history = message.channel.history(limit=10)
             for i in range(10):
@@ -63,8 +73,7 @@ class PinHandlerCog(commands.Cog, name='PinHandler'):
                     await sys_msg.delete()
                     break # No need to look further.
 
-            replystr = 'The following message was just pinned by %s:\n'
-            await channel.send(replystr % (author), embed=pinned_message)
+            await channel.send(f"The following message was just pinned by {author}:\n", embed=pinned_message)
 
 def setup(bot):
     bot.add_cog(PinHandlerCog(bot))
