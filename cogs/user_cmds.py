d7575de578c153237fd2e2e7a81a0be6aad86ab5
Alexander <terminalnode@protonmail.com>
2019-04-18 19:27:41 +0200

    Removed a lot of fluff from the !mrfreeze function

--- cogs/user_cmds.py
+++ cogs/user_cmds.py
@@ -28,50 +28,12 @@ class UserCmdsCog(commands.Cog, name='Everyone'):
     @commands.command(name='mrfreeze', aliases=['freeze'])
     async def _mrfreeze(self, ctx, *args):
         """Get a quote from my timeless classic "Batman & Robin"!"""
-        # If they're asking for help, we'll default to the help_reply.
-        pls_help = ('help', 'wtf', 'what', 'what\'s', 'wut', 'woot')
-        help_reply = False
-        for i in pls_help:
-            if i in args:
-                help_reply = True
+        with open('config/mrfreezequotes', 'r') as f:
+            quote = random.choice(f.read().strip().split('\n'))
 
-        # If they say the bot sucks, we'll default to the suck_reply.
-        you_suck = ('sucks', 'suck', 'blows', 'blow')
-        suck_reply = False
-        for i in you_suck:
-            if i in args:
-                suck_reply = True
-
-        # If they're telling the bot to kill itself... well...
-        death_reply = False
-        passive_die = ('die', 'suicide')
-        for i in args:
-            if i in passive_die:
-                death_reply = True
-
-        active_die  = ('kill', 'murder', 'destroy')
-        yourself_spells = ('yourself', 'urself', 'uself', 'u', 'you')
-        for i in active_die:
-            for k in yourself_spells:
-                if i and k in args:
-                    death_reply = True
-
-        if help_reply:
-            replystr = ('Allow me to break the ice: My name is Freeze and the command **!mrfreeze** ' +
-                        ' will have me print one of my timeless quotes from Batman & Robin.')
-            await ctx.send(replystr)
-        elif suck_reply:
-            replystr = 'Freeze in hell, %s!'
-            await ctx.send(replystr % (ctx.author.mention,))
-        elif death_reply:
-            replystr = '%s You\'re not sending *ME* to the *COOLER*!'
-            await ctx.send(replystr % (ctx.author.mention,))
-        else:
-            with open('config/mrfreezequotes', 'r') as f:
-                quote = random.choice(f.read().strip().split('\n'))
-            quote = quote.replace('Batman', ctx.author.mention)
-            quote = quote.replace('Gotham', ('**' + ctx.guild.name + '**'))
-            await ctx.send(quote)
+        quote = quote.replace('Batman', ctx.author.mention)
+        quote = quote.replace('Gotham', ('**' + ctx.guild.name + '**'))
+        await ctx.send(quote)
 
 
     @commands.command(name='cointoss', aliases=['coin', 'coinflip'])
