f947ef100db8b047e1338ddd9f1bb2572107d3ac
Alexander <terminalnode@protonmail.com>
2019-04-18 21:34:52 +0200

    Converted avatar to png and added mention to departure message.

--- cogs/departure_handling.py
+++ cogs/departure_handling.py
@@ -9,14 +9,18 @@ class DepartureHandlerCog(commands.Cog, name='DepartureHandler'):
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         mod_channel = discord.utils.get(member.guild.channels, name='mod-discussion')
-        member_name = str(member.name + '#' + str(member.discriminator))
-        member_count = str(len(member.guild.members))
 
         embed = discord.Embed(color=0x00dee9)
-        embed.set_thumbnail(url=member.avatar_url)
-        embed.add_field( name='A member has left the server! :sob:',
-                         value=('**%s#%s** is a smudgerous trech who\'s turned their back on %s. We now have only %s members.' %
-                         (member.name, str(member.discriminator), member.guild.name, member_count)) )
+        embed.set_thumbnail(url=member.avatar_url_as(static_format="png"))
+        embed.add_field(
+            name="A member has left the server! :sob:",
+            value=(
+                f"**{member.name}#{member.discriminator}** ({member.mention}) is a smudgerous trech " +
+                f"who's turned their back on {member.guild.name}. " +
+                f"We now have only {len(member.guild.members)} members."
+            )
+        )
+
         await mod_channel.send(embed=embed)
 
 def setup(bot):
