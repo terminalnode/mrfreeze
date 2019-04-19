694aead379ffac74e8acf58b9a0441b3b691af43
Alexander <terminalnode@protonmail.com>
2019-04-19 04:27:27 +0200

    Added command !getfreeze

--- README.md
+++ README.md
@@ -30,12 +30,13 @@ This is a rewrite of MrFreeze using cogs instead of mashing everything into a si
 
 ### Silly commands
 * **!activity**   - Changes the activity of the bot ('Playing [...]', 'Listening to [...]' etc). No mod requirement, have fun!
-* **!dummies**    - Invite links for the two dummy bots.
 * **!mrfreeze**   - Posts a MrFreeze-quote with user instead of Batman and server instead of Gotham.
 * Commands which post (more or less) useful links
 * * **!readme**    - Posts a link to the github page as well, but to the README.md file (this one!).
 * * **!source**    - Posts a link to this github page.
 * * **!todo**      - Posts a link to the TODO list for the bot.
+* * **!getfreeze** - Posts a link for inviting MrFreeze to your server.
+* * **!dummies**   - Posts invite links for the two dummy bots.
 
 ### Non-command features
 * Will greet a server when coming online.
--- cogs/about.py
+++ cogs/about.py
@@ -32,17 +32,29 @@ class AboutCog(commands.Cog, name='About'):
                                '[Gitlab](https://gitlab.com/terminalnode/MrFreeze/)!')
         await ctx.send(embed=source)
 
+    @commands.command(name='getfreeze', aliases=['getfrozen', 'getmrfreeze', 'freezemeup', 'freezeme'])
+    async def _getfreeze(self,ctx):
+        """Get an invite link to invite MrFreeze to your server!"""
+        mrfreeze = discord.Embed(color=0x00dee9,
+                                description = (f"Here's a link for inviting me, **{self.bot.user.name}**, to a server." +
+                                               f"The link will invite *this* version of me, be it the beta or regular version. Not the impostor.\n\n" +
+                                               f"Note that **!banish** and **!region** won't function properly without the right infrastructure.")
+            )
+        mrfreeze.add_field(name=f"Invite link for {self.bot.user.name}",
+                          value=(f"[Invite {self.bot.user.name} to a server.](https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot)")
+            )
+        await ctx.send(embed=mrfreeze)
 
     @commands.command(name='dummies')
     async def _dummies(self, ctx, *args):
         """Supplies you with links to invite Ba'athman and Robin."""
         dummies = discord.Embed(color=0x00dee9,
-                                description = ('Here are the links for inviting Ba\'athman and Robin, my arch enemies, to a server.\n\n' +
-                                               'They don\'t do anything at all, but are very useful for testing kick and ban commands.'))
+                                description = ("Here are the links for inviting Ba'athman and Robin, my arch enemies, to a server.\n\n" +
+                                               "They don't do anything at all, but are very useful for testing kick and ban commands."))
         dummies.set_thumbnail(url=native.get_image('Dummies'))
         dummies.add_field(name='Dummy Bots',
-                          value=('[Invite Ba\'athman to a server.](https://discordapp.com/oauth2/authorize?client_id=469030362119667712&scope=bot)\n' +
-                                 '[Invite Robin to a server.](https://discordapp.com/oauth2/authorize?client_id=469030900492009472&scope=bot)'))
+                          value=("[Invite Ba'athman to a server.](https://discordapp.com/oauth2/authorize?client_id=469030362119667712&scope=bot)\n" +
+                                 "[Invite Robin to a server.](https://discordapp.com/oauth2/authorize?client_id=469030900492009472&scope=bot)"))
 
         await ctx.send(embed=dummies)
 
