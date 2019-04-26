7b9a43e18c1e72b50845caa403c5d823027e04f4
Alexander <terminalnode@protonmail.com>
2019-04-26 04:32:23 +0200

    Images are now hosted locally and uploaded when necessery.

--- cogs/about.py
+++ cogs/about.py
@@ -14,59 +14,83 @@ class AboutCog(commands.Cog, name='About'):
     @commands.command(name='readme')
     async def _readme(self, ctx):
         """Hands you a link to the readme file over on Gitlab."""
-        readme = discord.Embed(color=0x00dee9)
-        readme.set_thumbnail(url=native.get_image('ReadMe'))
-        readme.add_field(name = 'Readme',
-                         value = ('There may be more information here than you\'re looking for, ' +
-                                 'but my readme-file is available [here on Gitlab](https://gitlab.com/terminalnode/MrFreeze/blob/master/README.md)'))
-        await ctx.send(embed=readme)
+        url = "https://gitlab.com/terminalnode/MrFreeze/blob/master/README.md"
+
+        embed = discord.Embed(color=0x00dee9)
+        embed.add_field(
+            name = "Readme",
+            value = f"My readme file is available [on Gitlab]({url})")
+
+        image = discord.File("images/readme.png")
+        embed.set_thumbnail(url="attachment://readme.png")
+        await ctx.send(embed=embed, file=image)
 
 
     @commands.command(name='source', aliases=['github', 'gitlab', 'git'])
     async def _source(self, ctx):
         """Hands you a link to the MrFreeze repository over on Gitlab."""
-        source = discord.Embed(color=0x00dee9)
-        source.set_thumbnail(url=native.get_image('Source'))
-        source.add_field(name='Source code',
-                         value='My source code is available on ' +
-                               '[Gitlab](https://gitlab.com/terminalnode/MrFreeze/)!')
-        await ctx.send(embed=source)
+        embed = discord.Embed(color=0x00dee9)
+        embed.add_field(
+            name="Source code",
+            value="My source code is available on [Gitlab](https://gitlab.com/terminalnode/MrFreeze/)!")
+
+        image = discord.File("images/source.png")
+        embed.set_thumbnail(url="attachment://source.png")
+        await ctx.send(embed=embed, file=image)
 
     @commands.command(name='getfreeze', aliases=['getfrozen', 'getmrfreeze', 'freezemeup', 'freezeme'])
     async def _getfreeze(self,ctx):
         """Get an invite link to invite MrFreeze to your server!"""
-        mrfreeze = discord.Embed(color=0x00dee9,
-                                description = (f"Here's a link for inviting me, **{self.bot.user.name}**, to a server." +
-                                               f"The link will invite *this* version of me, be it the beta or regular version. Not the impostor.\n\n" +
-                                               f"Note that **!banish** and **!region** won't function properly without the right infrastructure.")
-            )
-        mrfreeze.add_field(name=f"Invite link for {self.bot.user.name}",
-                          value=(f"[Invite {self.bot.user.name} to a server.](https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot)")
-            )
-        await ctx.send(embed=mrfreeze)
+        botname = self.bot.user.name
+        url     = f"https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot"
+
+        embed = discord.Embed(
+            color = 0x00dee9,
+            description = (
+                f"Here's a link for inviting me, **{botname}**, to a server." +
+                f"The link will invite *this* version of me, be it the beta or regular version. Not the impostor.\n\n" +
+                f"Note that **!banish** and **!region** won't function properly without the right infrastructure."))
+        embed.add_field(
+            name=f"Invite link for {botname}",
+            value=(f"[Invite {botname} to a server.]({url})"))
+
+        botpfp  = self.bot.user.avatar_url_as(static_format='png')
+        embed.set_thumbnail(url=botpfp)
+        await ctx.send(embed=embed)
 
     @commands.command(name='dummies')
     async def _dummies(self, ctx):
         """Supplies you with links to invite Ba'athman and Robin."""
-        dummies = discord.Embed(color=0x00dee9,
-                                description = ("Here are the links for inviting Ba'athman and Robin, my arch enemies, to a server.\n\n" +
-                                               "They don't do anything at all, but are very useful for testing kick and ban commands."))
-        dummies.set_thumbnail(url=native.get_image('Dummies'))
-        dummies.add_field(name='Dummy Bots',
-                          value=("[Invite Ba'athman to a server.](https://discordapp.com/oauth2/authorize?client_id=469030362119667712&scope=bot)\n" +
-                                 "[Invite Robin to a server.](https://discordapp.com/oauth2/authorize?client_id=469030900492009472&scope=bot)"))
+        baathman_url = "https://discordapp.com/oauth2/authorize?client_id=469030362119667712&scope=bot"
+        robin_url    = "https://discordapp.com/oauth2/authorize?client_id=469030900492009472&scope=bot"
 
-        await ctx.send(embed=dummies)
+        embed = discord.Embed(
+            color = 0x00dee9,
+            description = (
+                "Here are the links for inviting Ba'athman and Robin, my arch enemies, to a server.\n\n" +
+                "They don't do anything at all, but are very useful for testing kick and ban commands."))
+        embed.add_field(
+            name='Dummy Bots',
+            value=(f"[Invite Ba'athman to a server.]({baathman_url})\n" +
+                f"[Invite Robin to a server.]({robin_url})"))
+
+        image = discord.File("images/dummies.png")
+        embed.set_thumbnail(url="attachment://dummies.png")
+        await ctx.send(embed=embed, file=image)
 
     @commands.command(name='todo', aliases=['TODO', 'TODOs', 'ToDo', 'ToDos', 'todos', 'whatsnext', 'whatnext'])
     async def _todos(self, ctx):
         """Supplies you with a list of my planned features."""
-        todos = discord.Embed(color=0x00dee9)
-        todos.set_thumbnail(url=native.get_image('TODOs'))
-        todos.add_field(name="You're a nosy one! Here's a list of all the 'cool' stuff Terminal's got planned for me... :sleeping:",
-                        value="https://gitlab.com/terminalnode/MrFreeze/blob/master/TODO.md")
+        url = "https://gitlab.com/terminalnode/MrFreeze/blob/master/TODO.md"
 
-        await ctx.send(embed=todos)
+        embed = discord.Embed(color=0x00dee9)
+        embed.add_field(
+            name="TODO list",
+            value=f"You're a nosy one! [Here]({url})'s a list of all the \"cool\" stuff Terminal has planned for me... :sleeping:")
+
+        image = discord.File("images/todos.png")
+        embed.set_thumbnail(url="attachment://todos.png")
+        await ctx.send(embed=embed, file=image)
 
 def setup(bot):
     bot.add_cog(AboutCog(bot))
