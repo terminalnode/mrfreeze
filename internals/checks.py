314c6a0ae9e5594519fba17b76c2e4b239f3b918
Alexander <terminalnode@protonmail.com>
2019-06-24 20:49:40 +0200

    internals/checks: simplified checks and made them less server-dependent

--- internals/checks.py
+++ internals/checks.py
@@ -4,30 +4,36 @@ from . import native
 
 async def is_owner(ctx):
     # @commands.check(checks.is_owner)
-    if ctx.author.id != 154516898434908160:
-        await ctx.send(ctx.author.mention + ' You\'re not the boss of me, only Terminal is allowed to issue that command.')
-    return ctx.author.id == 154516898434908160
+    is_owner = await ctx.bot.is_owner(ctx.author)
+    if not is_owner:
+        await ctx.send(f"{ctx.author.mention} You're not the boss of me! <@!154516898434908160> Help I'm being opressed!!")
+    return is_owner
 
 async def is_mod(ctx):
     # @commands.check(checks.is_mod)
     # This check can also be used as a command with a member object,
     # depending on the input, output may vary.
+    member_call = isinstance(ctx, discord.member.Member)
 
-    if isinstance(ctx, discord.member.Member):
-        mod_status = (discord.utils.get(ctx.guild.roles, name='Administration') in ctx.roles)
+    if member_call:
+        mod_status = ctx.guild_permissions.administrator
 
-    elif isinstance(ctx, discord.ext.commands.context.Context):
-        mod_status = (discord.utils.get(ctx.guild.roles, name='Administration') in ctx.author.roles)
-
-        if ctx.command.name == 'mute' and not mod_status:
-            await native.punish_banish(ctx)
-
-        elif not mod_status:
-            await ctx.send(ctx.author.mention + ' Only mods are allowed to use that command.')
+    else:
+        if ctx.guild == None:
+            await ctx.send(f"Don't you try to sneak into my DMs and mod me!")
+            return False
+        mod_status = ctx.author.guild_permissions.administrator
+        if not mod_status:
+            await ctx.send(f"{ctx.author.mention} Only mods are allowed to use that command.")
 
     return mod_status
 
 async def always_deny(ctx):
     # @commands.check(checks.always_deny)
-    # This is for debugging, always returns false.
+    # This is for debugging, always returns False.
     return False
+
+async def always_allow():
+    # @commands.check(checks.always_deny)
+    # This is for debugging, always returns True.
+    return True
