1d5865aed07c2f905c0bd7fdf66d27b65ae2e1f0
Alexander <terminalnode@protonmail.com>
2019-06-27 00:19:20 +0200

    __main__: small changes in setup

--- __main__.py
+++ __main__.py
@@ -1,28 +1,28 @@
-import discord
-import traceback, sys, signal
-# from bot import *
-from internals import *
+from internals import mrfreeze  # The bot class itself
+import traceback                # Debugging loading of cogs
+import sys                      # Printing to stderr, exiting etc
+import signal                   # Intercepting Ctrl+C for a clean-ish exit
 
 # Instantiate the bot.
 bot = mrfreeze.MrFreeze(command_prefix=['!'])
 
 # Loading all the cogs.
 # One line for each cog makes it easy to disable.
-load_cogs = [   # Mostly for owner/mods.
-                'cogs.maintenance',    # Owner-only commands
-                'cogs.moderation',     # Mod-only commands.
-                'cogs.about',          # !dummies, !readme, !source
-
-                # Functions directed towards regular users.
-                # 'cogs.quotes',         # !quote
-                'cogs.user_cmds',      # Various smaller commands: !vote, !mrfreeze
-
-                # Silent(ish) bot functions.
-                'cogs.inkcyclopedia',      # Listen for inkzzz.
-                'cogs.error_handling',     # How the bot deals with errors.
-                'cogs.message_handling',   # How the bot deals with messages.
-                'cogs.departure_handling', # How the bot deals with departures.
-                'cogs.pin_handling',       # How the bot handles pins.
+load_cogs = [
+    # Silent(ish) bot functions.
+    'cogs.command_log',         # Logs all executed commands so you don't have to (as much).
+    'cogs.inkcyclopedia',       # Listen for inkzzz.
+    'cogs.error_handling',      # How the bot deals with errors.
+    'cogs.temp_converter',      # How the bot deals with messages.
+    'cogs.departures',          # How the bot deals with departures.
+    'cogs.pin_handling',        # How the bot handles pins.
+    # Mostly for owner/mods
+    'cogs.maintenance',         # Owner-only commands
+    'cogs.moderation',          # Mod-only commands.
+    'cogs.banish',              # Banish command.
+    # Functions directed towards regular users.
+    'cogs.user_cmds',           # Various smaller commands: !vote, !mrfreeze
+    'cogs.about',               # !dummies, !readme, !source
 ]
 
 # Actual loading of cogs take place here.
@@ -47,9 +47,11 @@ except:
            'This file should be located in the same directory as the bot files.\n')
     sys.exit(0)
 
-# Graceful exit
+# Graceful-ish exit
+# For some reason we need two or more ctrl+c to exit, but whatever
 def signal_handler(sig, frame):
-        print('\n\nYou pressed Ctrl+C!\nI will now do like the tree, and get out of here.')
+        print('\n\nYou pressed Ctrl+C!\nI will now do like the tree, and get the hell out of here.')
         sys.exit(0)
+
 signal.signal(signal.SIGINT, signal_handler)
 signal.pause()
