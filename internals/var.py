abdae75ac86ebda448e61e917e28b72867bdf332
Alexander <terminalnode@protonmail.com>
2019-06-29 22:12:24 +0200

    internals/var: no longer necessary, deleted

--- internals/var.py
+++
@@ -1,9 +0,0 @@
-# Various variables used throughout the program.
-# Colors
-boldcyan    = '\033[36;01m'
-boldwhite   = '\033[37;01m'
-white       = '\033[00;37m'
-cyan        = '\033[00;36m'
-green       = '\033[32;01m'
-red         = '\033[31;01m'
-reset       = '\033[00000m'
