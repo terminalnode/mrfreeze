4e7d4d8e2267046d9ab4c258887c8a865e9fd278
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-11-22 20:33:57 +0100

    fetching inks from airtable kind of works now

--- cogs/inkcyclopedia.py
+++ cogs/inkcyclopedia.py
@@ -1,5 +1,8 @@
-import discord  # Basic discord functionality
-import csv      # Required for parsing the list of inks
+import discord                  # Basic discord functionality
+import csv                      # Required for parsing the list of inks
+import os                       # To check if an inkfile already exists
+from airtable import airtable   # To update the inkdatabase, which is stored on airtable
+from internals import checks    # Only owner is allowed to update the ink database
 
 # Small script listening to all incoming messages looking for
 # mentions of inks. Based on The Inkcyclopedia by klundtasaur:
@@ -12,25 +15,68 @@ class InkcyclopediaCog(discord.ext.commands.Cog, name='Inkcyclopedia'):
     def __init__(self, bot):
         self.bot = bot
         self.inkydb = list()
+        self.inkdb_path = "databases/dbfiles/inkcyclopedia.csv"
+        self.inkdb_enc = "utf-8-sig"
+        self.airtable = None
+
+        # File config/airtable should have format:
+        # base = <your base id here>
+        # table = <your table name here>
+        # apikey = <your api_key here>
+        try:
+            with open("config/airtable", "r") as airtable_file:
+                content = [ i.split("=") for i in airtable_file.readlines() ]
+                content = [ [i[0].strip(), i[1].strip()] for i in content ]
+                keys = { i[0] : i[1] for i in content }
+                self.airtable = airtable.Airtable(
+                        keys["base"],
+                        keys["table"],
+                        api_key = keys["apikey"]
+                )
+        except:
+            self.cog_unload()
 
     @discord.ext.commands.Cog.listener()
     async def on_ready(self):
-        # Load up the ink db!
-        with open('databases/dbfiles/inkcyclopedia.csv', encoding='utf-8-sig') as inkfile:
-            inkdb = csv.reader(inkfile)
+        # Fetch inks if db does not exist
+        if not os.path.isfile(self.inkdb_path):
+            await self.fetch_inks()
 
-            for row in inkdb:
-                status   = row[3]
-                if 'Active' in status:
-                    ink_name = row[0]
-                    donator  = row[2]
-                    regex    = row[4]
-                    url      = row[5]
-                    self.inkydb.append((ink_name, regex, url, donator))
-            del inkdb
+        # Load up the ink db!
+        await self.update_db()
 
         # Print that the ink database has been loaded and with how many inks.
         print('\033[0;36mThe ink database has been loaded with \033[35;1m{} inks\033[0;36m!\033[0m'.format(str(len(self.inkydb))))
+
+    async def fetch_inks(self):
+        with open(self.inkdb_path, "w", encoding=self.inkdb_enc) as inkfile:
+            fetch = self.airtable.get_all()
+            writer = csv.writer(inkfile)
+            for row in fetch:
+                try:
+                    fields = row["fields"]
+                    to_file = [ fields["Ink Name"], fields["RegEx"], fields["Inkbot version"] ]
+                    writer.writerow(to_file)
+                except:
+                    pass
+
+    async def update_db(self):
+        with open(self.inkdb_path, encoding=self.inkdb_enc) as inkfile:
+            reader = csv.reader(inkfile)
+
+            for row in reader:
+                ink_name = row[0]
+                regex    = row[1]
+                url      = row[2]
+                self.inkydb.append((ink_name, regex, url))
+
+
+    @discord.ext.commands.command(name="inkupdate")
+    @discord.ext.commands.check(checks.is_owner)
+    async def inkupdate(self, ctx):
+        await self.fetch_inks()
+        await self.update_db()
+        await ctx.send(f"There are now {len(self.inkydb)} inks in the database!")
 
     @discord.ext.commands.Cog.listener()
     async def on_message(self, message):
