502be232bdddb87cb72f15fda420d3f0b6a80a9b
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-11-26 16:58:32 +0100

    maintenance cog type hinted and pep8:ed

--- cogs/maintenance.py
+++ cogs/maintenance.py
@@ -1,54 +1,72 @@
 import discord                      # Basic discord functionality
-import sys, os                      # Required to restart the bot
-from subprocess import run, PIPE    # Required to run shell commands
-from internals import checks        # Required to check that only bot owner can run commands
+# Required for bot restart
+import sys
+import os
+# Required to run shell commands
+from subprocess import run, PIPE
+# Required to check that only bot owner can run commands
+from internals import checks
+from internals.cogbase import CogBase
+
+# Some imports used in type hints
+from discord.ext.commands.context import Context
+from internals.mrfreeze import MrFreeze
+from typing import Tuple
+
 
 # This cog is for commands restricted to the owner of the bot (me!).
 # It has features like !restart and !gitupdate.
 def setup(bot):
-    bot.add_cog(MaintenanceCog(bot))
+    bot.add_cog(Maintenance(bot))
 
-class MaintenanceCog(discord.ext.commands.Cog, name='Maintenance'):
-    """There is only one person who is allowed to execute these commands... and it's not you."""
-    def __init__(self, bot):
+
+class Maintenance(CogBase):
+    """
+    There is only one person who is allowed to execute these commands...
+    and it's probably not you.
+    """
+    def __init__(self, bot: MrFreeze):
         self.bot = bot
 
-    @discord.ext.commands.command(name='restart')
+    @discord.ext.commands.command(name="restart")
     @discord.ext.commands.check(checks.is_owner)
-    async def _restart(self, ctx, *args):
-        """Makes me restart if and only if you're Terminal."""
-        await ctx.send(ctx.author.mention + " Yes Dear Leader... I will restart now.")
-        print ('\n') # extra new line after the commandlog() output
+    async def _restart(self, ctx: Context, *args: Tuple[str]) -> None:
+        """
+        Makes me restart if and only if you're Terminal.
+        """
+        mention: str = ctx.author.mention
+        await ctx.send(f"{mention} Yes Dear Leader... I will restart now.")
+        print("\n")  # extra new line after bot was restarted
         os.execl(sys.executable, sys.executable, *sys.argv)
 
-    @discord.ext.commands.command(name='update')
+    @discord.ext.commands.command(name="update")
     @discord.ext.commands.check(checks.is_owner)
-    async def _gitupdate(self, ctx, *args):
-        """Makes me update myself if and only if you're Terminal."""
+    async def _gitupdate(self, ctx: Context, *args: Tuple[str]) -> None:
+        """
+        Makes me update myself if and only if you're TerminalNode.
+        """
         # git fetch returns nothing if no updates were found
         # for some reason the output of git fetch is posted to stderr
-        gitfetch = str(run(['git', 'fetch'], stderr=PIPE, encoding='utf_8').stderr)
-        gitpull = str(run(['git', 'pull'], stdout=PIPE, encoding='utf_8').stdout)
-        output = str()
+        gitfetch: str = str(
+            run(["git", "fetch"], stderr=PIPE, encoding="utf_8").stderr
+        )
+        gitpull: str = str(
+            run(["git", "pull"], stdout=PIPE, encoding="utf_8").stdout
+        )
 
-        if gitfetch == '':
-            gitfetch = 'No output.'
+        if gitfetch == "":
+            gitfetch = "No output."
 
-        if gitpull != 'Already up-to-date.':
+        # git seems to like to change how this message is written.
+        do_restart: bool = False
+        if "alreadyuptodate" not in gitpull.lower().replace("-", "").replace(" ", ""):
             do_restart = True
-        else:
-            do_restart = False
 
-        output += '**git fetch:**\n'
-        output += gitfetch + '\n\n'
-        output += '**git pull:**\n'
-        output += gitpull
-
-        await ctx.author.send(output)
-
-        if do_restart:
-            await ctx.send('Updates retrieved, restarting.')
+        output: str = (f"**git fetch:**\n{gitfetch}\n\n" +
+                       f"**git pull:**\n{gitpull}")
 
         # If an update was found, restart automatically.
+        await ctx.author.send(output)
         if do_restart:
+            await ctx.send("Updates retrieved, restarting.")
             os.execl(sys.executable, sys.executable, *sys.argv)
