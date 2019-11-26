20b2468f95a0e04df9e776ea951154f5a82b56b9
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-11-26 16:39:34 +0100

    departures cog type hinted and pep8:ed

--- cogs/departures.py
+++ cogs/departures.py
@@ -1,26 +1,41 @@
 import discord
+from discord import Embed
+from internals.cogbase import CogBase
+
+# Some imports used in type hints
+from discord import Member, TextChannel
+from internals.mrfreeze import MrFreeze
+from typing import List
+
 
 def setup(bot):
-    bot.add_cog(DeparturesCog(bot))
+    bot.add_cog(Departures(bot))
 
-class DeparturesCog(discord.ext.commands.Cog, name='Departures'):
-    """How the bot acts when members leave the chat."""
-    def __init__(self, bot):
+
+class Departures(CogBase):
+    """
+    How the bot acts when members leave the chat.
+    """
+    def __init__(self, bot: MrFreeze):
         self.bot = bot
 
-    @discord.ext.commands.Cog.listener()
-    async def on_member_remove(self, member):
-        mod_channel = discord.utils.get(member.guild.channels, name='mod-discussion')
+    @CogBase.listener()
+    async def on_member_remove(self, member: Member):
+        guild_channels: List[TextChannel] = member.guild.text_channels
+        mod_channel: TextChannel = discord.utils.get(
+                guild_channels,
+                name="mod-discussion")
+        mention: str = member.mention
+        username: str = f"{member.name}#{member.discriminator}"
 
-        embed = discord.Embed(color=0x00dee9)
+        embed = Embed(color=0x00dee9)
         embed.set_thumbnail(url=member.avatar_url_as(static_format="png"))
         embed.add_field(
-            name=f"{member.name}#{member.discriminator} has left the server! :sob:",
+            name=f"{username} has left the server! :sob:",
             value=(
-                f"{member.mention} is a smudgerous trech " +
-                f"who's turned their back on {member.guild.name}.\n" +
-                f"We now have only {len(member.guild.members)} members."
+                f"{mention} is a smudgerous trech " +
+                f"who's turned their back on {member.guild.name}.\n\n" +
+                f"We're now down to {len(member.guild.members)} members."
             )
         )
-
         await mod_channel.send(embed=embed)
