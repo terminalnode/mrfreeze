24b53cfdfa6db2b25f17dadb698a27b24b97f59b
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-11-27 19:34:24 +0100

    error handling cog type hinted and pep8:ed

--- cogs/error_handling.py
+++ cogs/error_handling.py
@@ -1,71 +1,112 @@
-import discord      # Required to check for a lot of different discord exceptions
-import asyncio      # Required to check for asyncio.TimeoutErrors
-import traceback    # Debugging
-import sys          # Debugging
+import inflect    # Used to convert number to word
+import traceback  # Debugging
+import sys        # Debugging
+from internals.cogbase import CogBase
 
-# If buckets are ever implemented again:
-# from discord.ext.commands.cooldowns import BucketType
-# import inflect
-def setup(bot):
-    bot.add_cog(ErrorHandlerCog(bot))
+# Various errors that we're going to be testing for
+from discord.ext.commands import CheckFailure, BadArgument, CommandInvokeError
+from discord.ext.commands import CommandOnCooldown, CommandNotFound
+from discord.ext.commands.cooldowns import BucketType
 
-class ErrorHandlerCog(discord.ext.commands.Cog, name='ErrorHandler'):
-    """How the bot acts when errors occur."""
+# Some imports used in type hints
+from discord.ext.commands.context import Context
+from internals.mrfreeze import MrFreeze
 
-    def __init__(self, bot):
+
+def setup(bot: MrFreeze):
+    bot.add_cog(ErrorHandler(bot))
+
+
+class ErrorHandler(CogBase):
+    """
+    How the bot acts when errors occur.
+    """
+    def __init__(self, bot: MrFreeze) -> None:
         self.bot = bot
+        self.initialize_colors()
 
-    @discord.ext.commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        """What happens when we encounter a command error? This happens."""
+    @CogBase.listener()
+    async def on_command_error(self, ctx: Context, error) -> None:
+        """
+        What happens when we encounter a command error? This happens.
+        """
+        time: str = self.current_time()
+        username: str = f"{ctx.author.name}#{ctx.author.discriminator}"
+        mention: str = ctx.author.mention
+        invocation: str = f"{self.WHITE_B}{ctx.prefix}{ctx.invoked_with}"
 
-        current_time = self.bot.current_time()
-        userstr = f"{ctx.author.name}#{ctx.author.discriminator}"
+        if isinstance(error, CheckFailure):
+            print(f"{time} {self.RED_B}Check failure: {self.CYAN}" +
+                  f"{username} tried to illegaly invoke {invocation}" +
+                  f"{self.RESET}")
 
-        if isinstance(error, discord.ext.commands.CheckFailure):
-            print(f"{current_time} {self.bot.RED_B}Check failure: {self.bot.CYAN}{userstr} tried to illegaly invoke {self.bot.WHITE_B}!{ctx.invoked_with}{self.bot.RESET}")
+        elif isinstance(error, BadArgument):
+            print(f"{time} {self.RED_B}Bad arguments: {self.CYAN}{username} " +
+                  f"while using command {invocation}{self.RESET}")
+            await ctx.send(f"{mention} That's not quite the information I " +
+                           "need to execute that command.")
 
-        elif isinstance(error, discord.ext.commands.errors.BadArgument):
-            print(f"{current_time} {self.bot.RED_B}Bad arguments: {userstr} while using command {self.bot.WHITE_B}!{ctx.invoked_with}{self.bot.RESET}")
-            await ctx.send(f"{ctx.author.mention} That's not quite the information I need to execute that command.")
+        elif isinstance(error, CommandNotFound):
+            print(f"{time} {self.RED_B}Command not found: {self.CYAN}" +
+                  f"{username} tried to use {invocation}{self.RESET}")
 
-        elif isinstance(error, discord.ext.commands.errors.CommandNotFound):
-            print(f"{current_time} {self.bot.RED_B}Command not found: {self.bot.CYAN}{userstr} tried {self.bot.WHITE_B}!{ctx.invoked_with}{self.bot.RESET}")
+        elif isinstance(error, CommandInvokeError):
+            # On Timeout Error, a CommandInvokeError containing the original
+            # error is returned. Not the original TimeoutError itself.
+            error_name: str = type(error.original).__name__
+            print(f"{time} {self.RED_B}{error_name}: {self.CYAN}" +
+                  f"{username} tried to use {invocation}{self.RESET}")
 
-        elif isinstance(error, asyncio.TimeoutError):
-            print(f"{current_time} {self.bot.RED_B}AsyncIO Timeout {self.bot.RESET}")
-
-        elif isinstance(error, discord.ext.commands.errors.CommandOnCooldown):
-            if error.cooldown.rate == 1:    er_rate = 'once'
-            elif error.cooldown.rate == 2:  er_rate = 'twice'
+        elif isinstance(error, CommandOnCooldown):
+            if error.cooldown.rate == 1:
+                er_rate = "once"
+            elif error.cooldown.rate == 2:
+                er_rate = "twice"
             else:
-                infl    = inflect.engine()
+                infl = inflect.engine()
                 er_rate = infl.number_to_words(error.cooldown.rate) + ' times'
 
-            er_per_sec   = int(error.cooldown.per)
+            er_per_sec = int(error.cooldown.per)
             er_retry_sec = int(error.retry_after)
 
             def seconds_parse(time_input):
                 if time_input > 60:
                     input_min = int(time_input / 60)
                     input_sec = int(time_input - (input_min * 60))
-                    if input_sec != 0:  return f"{input_min} min {input_sec} sec"
-                    else:               return f"{input_min} min"
-                else:                   return f"{input_sec} sec"
+                    if input_sec != 0:
+                        return f"{input_min} min {input_sec} sec"
+                    else:
+                        return f"{input_min} min"
+                else:
+                    return f"{er_retry_sec} sec"
 
             er_per = seconds_parse(er_per_sec)
             er_retry = seconds_parse(er_retry_sec)
 
             bucket = error.cooldown.type
-            if bucket == BucketType.default:    er_type = '' # Writing nothing is fine here.
-            elif bucket == BucketType.user:     er_type = ' by every user'
-            elif bucket == BucketType.guild:    er_type = ' in every server'
-            elif bucket == BucketType.channel:  er_type = ' in every channel'
-            elif bucket == BucketType.member:   er_type = ' by every user'
-            else:                               er_type = ' [missing bucket type]'
+            if bucket == BucketType.user:
+                er_type = " for every user"
+            elif bucket == BucketType.member:
+                er_type = " for every member"
+            elif bucket == BucketType.guild:
+                er_type = " in every server"
+            elif bucket == BucketType.channel:
+                er_type = " in every channel"
+            else:
+                er_type = ""
 
-            await ctx.send("{ctx.author.name} The command **{ctx.invoked_with}** can only be used {er_rate} every {er_per}{er_type}. Try again in {er_retry}.")
+            await ctx.send(f"{mention} The command **{ctx.prefix}" +
+                           f"{ctx.invoked_with}** can only be used " +
+                           f"{er_rate} every {er_per}{er_type}." +
+                           f"\nTry again in {er_retry}.")
 
         else:
-            print(f"{current_time} {self.bot.RED_B}Unclassified error: {self.bot.WHITE_B}{error}{self.bot.RESET}")
-            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+            # Who knows what happened? The stack trace, that's who.
+            print(f"{time} {self.RED_B}Unclassified error: " +
+                  f"{self.WHITE_B}{error}{self.RESET}")
+            traceback.print_exception(
+                    type(error),
+                    error,
+                    error.__traceback__,
+                    file=sys.stderr
+            )
