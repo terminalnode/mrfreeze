6292e25d1bc5e410e77f5219c933d82f1d0ec457
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-12-29 23:53:01 +0100

    full test coverage of checks

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -7,6 +7,8 @@ from mrfreeze import checks
 
 from tests import helpers
 
+# TODO
+# is_mod() doesn't yet test the response messages of guild-less ctx calls
 
 class ChecksUnitTest(unittest.TestCase):
     """Test the command checks."""
@@ -71,6 +73,21 @@ class ChecksUnitTest(unittest.TestCase):
         self.member.guild_permissions.administrator = False
         result = self.loop.run_until_complete(checks.is_mod(self.ctx))
 
+        self.ctx.send.assert_called_with(
+            "@member Only mods are allowed to use that command.")
+        self.assertFalse(result)
+
+    def test_is_mod_with_ctx_object_and_unauthorised_caller_in_dm(self):
+        """
+        Test is_mod() with context object when caller is NOT admin.
+
+        Should return guild_permissions.administrator (== False).
+        """
+        self.ctx.guild = None
+        result = self.loop.run_until_complete(checks.is_mod(self.ctx))
+
+        self.ctx.send.assert_called_with(
+            "Don't you try to sneak into my DMs and mod me!")
         self.assertFalse(result)
 
     def test_is_mod_with_member_object_and_authorised_caller(self):
@@ -92,5 +109,83 @@ class ChecksUnitTest(unittest.TestCase):
         """
         self.member.guild_permissions.administrator = False
         result = self.loop.run_until_complete(checks.is_mod(self.member))
+
+        self.assertFalse(result)
+
+    def test_always_allow_with_regular_user(self):
+        """
+        Test always_allow() with non-owner non-admin member.
+
+        Should return True.
+        """
+        self.ctx.bot.is_owner.return_value = False
+        self.member.guild_permissions.administrator = False
+
+        result = self.loop.run_until_complete(checks.always_allow(self.member))
+
+        self.assertTrue(result)
+
+    def test_always_allow_with_non_owner_admin(self):
+        """
+        Test always_allow() with non-owner admin member.
+
+        Should return True.
+        """
+        self.ctx.bot.is_owner.return_value = False
+        self.member.guild_permissions.administrator = True
+
+        result = self.loop.run_until_complete(checks.always_allow(self.member))
+
+        self.assertTrue(result)
+
+    def test_always_allow_with_owner(self):
+        """
+        Test always_allow() with owner non-admin member.
+
+        Should return True.
+        """
+        self.ctx.bot.is_owner.return_value = True
+        self.member.guild_permissions.administrator = False
+
+        result = self.loop.run_until_complete(checks.always_allow(self.member))
+
+        self.assertTrue(result)
+
+    def test_always_deny_with_regular_user(self):
+        """
+        Test always_deny() with non-owner non-admin member.
+
+        Should return False.
+        """
+        self.ctx.bot.is_owner.return_value = False
+        self.member.guild_permissions.administrator = False
+
+        result = self.loop.run_until_complete(checks.always_deny(self.member))
+
+        self.assertFalse(result)
+
+    def test_always_deny_with_non_owner_admin(self):
+        """
+        Test always_deny() with non-owner admin member.
+
+        Should return False.
+        """
+        self.ctx.bot.is_owner.return_value = False
+        self.member.guild_permissions.administrator = True
+
+        result = self.loop.run_until_complete(checks.always_deny(self.member))
+
+        self.assertFalse(result)
+
+    def test_always_deny_with_owner(self):
+        """
+        Test always_deny() with owner non-admin member.
+
+        Should return False.
+        """
+        self.ctx.bot.is_owner.return_value = True
+        self.member.guild_permissions.administrator = False
+
+        result = self.loop.run_until_complete(checks.always_deny(self.member))
 
         self.assertFalse(result)
