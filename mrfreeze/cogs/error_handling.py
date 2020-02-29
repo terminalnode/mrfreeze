1aa236ad9ea33b0b812a664e5bd4d54518728b4e
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-02-29 03:41:32 +0100

    add error handling for MissingArguments and type annotations

--- mrfreeze/cogs/error_handling.py
+++ mrfreeze/cogs/error_handling.py
@@ -1,20 +1,27 @@
+"""Cog for handling various errors that might arise when executing commands."""
+
 import sys
 import traceback
 
+from discord.ext.commands import BadArgument
+from discord.ext.commands import CheckFailure
+from discord.ext.commands import CommandInvokeError
+from discord.ext.commands import CommandNotFound
+from discord.ext.commands import CommandOnCooldown
+from discord.ext.commands import Context
+from discord.ext.commands import MissingRequiredArgument
+from discord.ext.commands.cooldowns import BucketType
+
 import inflect
 
+from mrfreeze import colors
 from mrfreeze.bot import MrFreeze
 from mrfreeze.checks import MuteCheckFailure
-from mrfreeze import colors
 
 from .cogbase import CogBase
 
-from discord.ext.commands import BadArgument, CheckFailure, CommandInvokeError
-from discord.ext.commands import CommandNotFound, CommandOnCooldown
-from discord.ext.commands.context import Context
-from discord.ext.commands.cooldowns import BucketType
 
-def setup(bot: MrFreeze):
+def setup(bot: MrFreeze) -> None:
     """Add the cog to the bot."""
     bot.add_cog(ErrorHandler(bot))
 
@@ -26,38 +33,53 @@ class ErrorHandler(CogBase):
         self.bot = bot
 
     @CogBase.listener()
-    async def on_command_error(self, ctx: Context, error) -> None:
-        """What happens when we encounter a command error? This happens."""
+    async def on_command_error(self, ctx: Context, error: Exception) -> None:
+        """Determine what should happen when we encounter a command error."""
         time: str = self.current_time()
         username: str = f"{ctx.author.name}#{ctx.author.discriminator}"
         mention: str = ctx.author.mention
         invocation: str = f"{colors.WHITE_B}{ctx.prefix}{ctx.invoked_with}"
 
         if isinstance(error, MuteCheckFailure):
-            print(f"{time} {colors.RED_B}MrFreeze is muted: {colors.CYAN}" +
-                  f"{error}{colors.RESET}")
+            status = f"{time} {colors.RED_B}MrFreeze is muted: {colors.CYAN}{error}{colors.RESET}"
+            print(status)
 
         elif isinstance(error, CheckFailure):
-            print(f"{time} {colors.RED_B}Check failure: {colors.CYAN}" +
-                  f"{username} tried to illegaly invoke {invocation}" +
-                  f"{colors.RESET}")
+            status  = f"{time} {colors.RED_B}Check failure: {colors.CYAN}"
+            status += f"{username} tried to illegaly invoke {invocation}{colors.RESET}"
+            print(status)
+
+        elif isinstance(error, MissingRequiredArgument):
+            print(error)
+            status  = f"{time} {colors.RED_B}Check failure: {colors.CYAN}"
+            status += f"{username} tried executing {invocation} with too "
+            status += f"few arguments{colors.RESET}"
+            print(status)
+
+            reply  = f"{ctx.author.mention} You need to specify some arguments to invoke "
+            reply += f"{ctx.prefix}{ctx.invoked_with}, or I won't know what to do."
+            await ctx.send(reply)
 
         elif isinstance(error, BadArgument):
-            print(f"{time} {colors.RED_B}Bad arguments: {colors.CYAN}{username} " +
-                  f"while using command {invocation}{colors.RESET}")
-            await ctx.send(f"{mention} That's not quite the information I " +
-                           "need to execute that command.")
+            status  = f"{time} {colors.RED_B}Bad arguments: {colors.CYAN}{username} "
+            status += f"while using command {invocation}{colors.RESET}"
+            print(status)
+
+            reply = f"{mention} That's not quite the information I need to execute that command."
+            await ctx.send(reply)
 
         elif isinstance(error, CommandNotFound):
-            print(f"{time} {colors.RED_B}Command not found: {colors.CYAN}" +
-                  f"{username} tried to use {invocation}{colors.RESET}")
+            status  = f"{time} {colors.RED_B}Command not found: {colors.CYAN}"
+            status += f"{username} tried to use {invocation}{colors.RESET}"
+            print(status)
 
         elif isinstance(error, CommandInvokeError):
             # On Timeout Error, a CommandInvokeError containing the original
             # error is returned. Not the original TimeoutError itself.
             error_name: str = type(error.original).__name__
-            print(f"{time} {colors.RED_B}{error_name}: {colors.CYAN}" +
-                  f"{username} tried to use {invocation}{colors.RESET}")
+            status  = f"{time} {colors.RED_B}{error_name}: {colors.CYAN}"
+            status += f"{username} tried to use {invocation}{colors.RESET}"
+            print(status)
 
         elif isinstance(error, CommandOnCooldown):
             if error.cooldown.rate == 1:
@@ -66,12 +88,12 @@ class ErrorHandler(CogBase):
                 er_rate = "twice"
             else:
                 infl = inflect.engine()
-                er_rate = infl.number_to_words(error.cooldown.rate) + ' times'
+                er_rate = infl.number_to_words(error.cooldown.rate) + " times"
 
             er_per_sec = int(error.cooldown.per)
             er_retry_sec = int(error.retry_after)
 
-            def seconds_parse(time_input):
+            def seconds_parse(time_input: int) -> str:
                 if time_input > 60:
                     input_min = int(time_input / 60)
                     input_sec = int(time_input - (input_min * 60))
@@ -102,18 +124,18 @@ class ErrorHandler(CogBase):
             else:
                 er_type = ""
 
-            await ctx.send(f"{mention} The command **{ctx.prefix}" +
-                           f"{ctx.invoked_with}** can only be used " +
-                           f"{er_rate} every {er_per}{er_type}." +
-                           f"\nTry again in {er_retry}.")
+            reply  = f"{mention} The command **{ctx.prefix}{ctx.invoked_with}** can only be used "
+            reply += f"{er_rate} every {er_per}{er_type}.\nTry again in {er_retry}."
+            await ctx.send(reply)
 
         else:
             # Who knows what happened? The stack trace, that's who.
-            print(f"{time} {colors.RED_B}Unclassified error: " +
-                  f"{colors.WHITE_B}{error}{colors.RESET}")
+            status  = f"{time} {colors.RED_B}Unclassified error: "
+            status += f"{colors.WHITE_B}{error}{colors.RESET}"
+            print(status)
             traceback.print_exception(
-                    type(error),
-                    error,
-                    error.__traceback__,
-                    file=sys.stderr
+                type(error),
+                error,
+                error.__traceback__,
+                file=sys.stderr
             )
