6b1a06ae7ed2873c6541a36265b8a16c81cc9f3b
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-02-29 01:18:04 +0100

    implement all methods for TrashChannels

--- mrfreeze/database/server_settings/trash_channels.py
+++ mrfreeze/database/server_settings/trash_channels.py
@@ -6,13 +6,27 @@ trash_channels      channel*   INTEGER      Channel ID
                     server*    INTEGER      Server ID
 """
 
-from ..helpers import db_create, db_connect, db_time, failure_print, success_print
+from typing import Dict
+from typing import Optional
+
+from discord import Guild
+from discord import TextChannel
+
+from ..helpers import db_create
+from ..helpers import db_execute
+from ..helpers import failure_print
+from ..helpers import success_print
+
 
 class TrashChannels:
-    def __init__(self, parent):
-        self.parent = parent
+    """Class for handling the trash_channels table."""
+
+    def __init__(self, dbpath: str) -> None:
+        self.dbpath = dbpath
         self.module_name = "Trash channels table"
         self.table_name = "trash_channels"
+        self.trash_channels: Optional[Dict[int, int]] = None
+
         self.table = f"""
         CREATE TABLE IF NOT EXISTS {self.table_name} (
             channel     INTEGER NOT NULL,
@@ -20,6 +34,93 @@ class TrashChannels:
             CONSTRAINT server_trash_channel PRIMARY KEY (channel, server)
         );"""
 
-    def initialize(self):
-        """Setup the trash channels table."""
-        db_create(self.parent.dbpath, self.module_name, self.table)
+    def initialize(self) -> None:
+        """Set up the trash channels table."""
+        db_create(self.dbpath, self.module_name, self.table)
+
+    def set_trash_channel(self, channel: TextChannel) -> bool:
+        """Set the trash channel for a given server using the channel object."""
+        return self.upsert(channel.guild, channel.id)
+
+    def set_trash_channel_by_id(self, server: Guild, channel_id: int) -> bool:
+        """Set the trash channel for a given server using the channel id."""
+        return self.upsert(server, channel_id)
+
+    def upsert(self, server: Guild, value: int) -> bool:
+        """Insert or update the value for `server.id` with `value`."""
+        sql = f"""INSERT INTO {self.table_name} (server, channel) VALUES (?, ?)
+              ON CONFLICT(server) DO UPDATE SET channel = ?;"""
+        query = db_execute(self.dbpath, sql, (server.id, value, value))
+
+        if not query.error:
+            failure_print(
+                self.module_name,
+                f"failed to set {server.name} to {value}\n{query.error}")
+            return False
+
+        elif not self.update_dictionary(server.id, value):
+            failure_print(
+                self.module_name,
+                f"failed to update dictionary for {server.name} to {value}\n{query.error}")
+            return False
+
+        else:
+            success_print(
+                self.module_name,
+                f"successfully set {server.name} to {value}")
+            return True
+
+    def get_trash_channel(self, server: Guild) -> Optional[int]:
+        """Retrieve the trash channel for a given server."""
+        # Check that current mutes has been fetched.
+        # If not refetch it.
+        if self.trash_channels is None:
+            self.trash_channels_from_db()
+
+        # Check that it was indeed fetched, otherwise
+        # return None as default.
+        if self.trash_channels is None:
+            return None
+
+        # Check if the requested server has an entry,
+        # otherwise return None as default.
+        if server.id not in self.trash_channels:
+            return None
+
+        # Finally, return the actual value from the dictionary.
+        return self.trash_channels[server.id]
+
+    def update_dictionary(self, key: int, value: int) -> bool:
+        """Try to update the dictionary with the trash channels."""
+        if self.trash_channels is None:
+            self.trash_channels_from_db()
+
+        if self.trash_channels is None:
+            return False
+        else:
+            self.trash_channels[key] = value
+            return True
+
+    def trash_channels_from_db(self) -> None:
+        """
+        Load current trash channel values from database.
+
+        The values are then stored in a dictionary for quick access.
+        Whenever the value is changed, the value in the dictionary and
+        the database are updated simultaneously through the upsert method.
+        """
+        query = db_execute(
+            self.dbpath,
+            f"SELECT server, channel FROM {self.table_name}",
+            tuple())
+
+        if not query.error:
+            new_dict = dict()
+            for entry in query.output:
+                new_dict[entry[0]] = entry[1]
+
+            self.trash_channels = new_dict
+            success_print(self.module_name, "successfully fetched trash channels")
+        else:
+            failure_print(self.module_name, f"failed to fetch trash channels: {query.error}")
+            self.trash_channels = None
