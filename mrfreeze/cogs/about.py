aa3129bde1386099c90675470c73df0a793011bf
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-07-18 07:00:11 +0200

    change readme link to point to wiki

--- mrfreeze/cogs/about.py
+++ mrfreeze/cogs/about.py
@@ -11,35 +11,33 @@ from mrfreeze.bot import MrFreeze
 from mrfreeze.cogs.cogbase import CogBase
 
 
-def setup(bot):
+def setup(bot: MrFreeze) -> None:
     """Add the cog to the bot."""
     bot.add_cog(About(bot))
 
 
 source_aliases: List[str] = [
-        "github", "gitlab", "git"]
+    "github", "gitlab", "git"]
 getfreeze_aliases: List[str] = [
-        "getfrozen", "getmrfreeze", "freezemeup", "freezeme"]
-todo_aliases: List[str] = [
-        "TODO", "TODOs", "ToDo", "ToDos", "todos", "whatsnext", "whatnext"]
+    "getfrozen", "getmrfreeze", "freezemeup", "freezeme"]
 
 
 class About(CogBase):
     """Use these commands to unlock my deepest, darkest inner secrets."""
 
-    def __init__(self, bot: MrFreeze):
+    def __init__(self, bot: MrFreeze) -> None:
         """Initialize the About cog."""
         self.bot = bot
 
-    @discord.ext.commands.command(name="readme")
+    @discord.ext.commands.command(name="wiki", aliases=[ "readme", "commands" ])
     async def readme(self, ctx: Context) -> None:
-        """Get a link to the readme file over on Github."""
-        url = "https://github.com/terminalnode/mrfreeze/blob/master/README.md"
+        """Get a link to the wiki over on Github."""
+        url = "https://github.com/terminalnode/mrfreeze/wiki"
 
         embed = Embed(color=0x00dee9)
         embed.add_field(
             name="Readme",
-            value=f"My readme file is available [on Github]({url})!")
+            value=f"Check out my wiki page [on Github]({url})!")
 
         image = File("images/readme.png")
         embed.set_thumbnail(url="attachment://readme.png")
@@ -49,11 +47,8 @@ class About(CogBase):
     async def source(self, ctx: Context) -> None:
         """Get a link to the MrFreeze repository over on Github."""
         embed = Embed(color=0x00dee9)
-        embed.add_field(
-            name="Source code",
-            value=("My source code is available [on " +
-                   "Github](https://github.com/terminalnode/mrfreeze)!")
-        )
+        text = "My source code is available [on Github](https://github.com/terminalnode/mrfreeze)!"
+        embed.add_field(name="Source code", value=text)
 
         image = File("images/source.png")
         embed.set_thumbnail(url="attachment://source.png")
@@ -63,17 +58,15 @@ class About(CogBase):
     async def getfreeze(self, ctx: Context) -> None:
         """Get an invite link to invite MrFreeze to your server."""
         botname = self.bot.user.name
-        url = ("https://discordapp.com/oauth2/authorize?" +
-               f"client_id={self.bot.user.id}&scope=bot")
+        url = "https://discordapp.com/oauth2/authorize?"
+        url += f"client_id={self.bot.user.id}&scope=bot"
 
-        embed = Embed(
-            color=0x00dee9,
-            description=(
-                f"Here's a link for inviting me, **{botname}**, to a server." +
-                "The link will invite *this* version of me, be it the beta " +
-                "or regular version. Not the impostor.\n\nNote however that " +
-                "**!banish** and **!region** won't work without the right " +
-                "server infrastructure (roles etc.)."))
+        text = f"Here's a link for inviting me, **{botname}**, to a server. "
+        text += "The link will invite *this* version of me.\n\nNote that "
+        text += "**!banish** and **!region** won't work without the right "
+        text += "server infrastructure (roles etc.)."
+
+        embed = Embed(color=0x00dee9, description=text)
         embed.add_field(
             name=f"Invite link for {botname}",
             value=(f"[Invite {botname} to a server]({url})"))
@@ -85,40 +78,22 @@ class About(CogBase):
     @discord.ext.commands.command(name="dummies")
     async def dummies(self, ctx: Context) -> None:
         """Get links to invite Ba'athman and Robin."""
-        baathman_url: str = ("https://discordapp.com/oauth2/authorize?" +
-                             "client_id=469030362119667712&scope=bot")
-        robin_url: str = ("https://discordapp.com/oauth2/authorize?" +
-                          "client_id=469030900492009472&scope=bot")
+        baathman_url = "https://discordapp.com/oauth2/authorize?"
+        baathman_url += "client_id=469030362119667712&scope=bot"
+        robin_url = "https://discordapp.com/oauth2/authorize?"
+        robin_url += "client_id=469030900492009472&scope=bot"
 
-        embed = Embed(
-            color=0x00dee9,
-            description=(
-                "Here are the links for inviting Ba'athman and Robin, my " +
-                "arch enemies, to a server.\n\nThey don't do anything " +
-                "whatsoever, but are very useful for testing purposes " +
-                "such as trying out kick and ban commands.")
-        )
-        embed.add_field(
-            name="Dummy Bots",
-            value=(f"[Invite Ba'athman to a server]({baathman_url})\n" +
-                   f"[Invite Robin to a server]({robin_url})"))
+        embed_text = "Here are the links for inviting Ba'athman and Robin, my "
+        embed_text += "arch enemies, to a server.\n\nThey don't do anything "
+        embed_text += "whatsoever, but are very useful for testing purposes "
+        embed_text += "such as trying out kick and ban commands."
+
+        invite_text = f"[Invite Ba'athman to a server]({baathman_url})\n"
+        invite_text += f"[Invite Robin to a server]({robin_url})"
+
+        embed = Embed(color=0x00dee9, description=embed_text)
+        embed.add_field(name="Dummy Bots", value=invite_text)
 
         image = File("images/dummies.png")
         embed.set_thumbnail(url="attachment://dummies.png")
-        await ctx.send(embed=embed, file=image)
-
-    @discord.ext.commands.command(name="todo", aliases=todo_aliases)
-    async def todos(self, ctx: Context) -> None:
-        """Get a list of planned features for the bot."""
-        url = "https://github.com/terminalnode/mrfreeze/blob/master/TODO.md"
-
-        embed = Embed(color=0x00dee9)
-        embed.add_field(
-            name="TODO list",
-            value=(f"You're a nosy one! [Here]({url})'s a list of all the " +
-                   "\"cool\" stuff Terminal has planned for me... :sleeping:")
-        )
-
-        image = File("images/todos.png")
-        embed.set_thumbnail(url="attachment://todos.png")
         await ctx.send(embed=embed, file=image)
